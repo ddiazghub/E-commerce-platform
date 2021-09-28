6ce8cb7494899efd97467760b7e72b598da531b5
ddiazghub <dedemoya@uninorte.edu.co>
2021-09-28 13:22:22 -0500

    Added setenv files 7

--- setenv.ps1
+++
@@ -1,6 +0,0 @@
-#In this file authentication credentials should be set as env variables. You can delete this file if using linux.
-$env:DB_HOST="server.com"
-$env:DB_USER="user"
-$env:DB_PORT="5432"
-$env:DB_PASSWORD="password"
-$env:DB_NAME="database"
--- setenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#In this file authentication credentials should be set as env variables. You can delete this file if using windows.
-export DB_HOST="server.com"
-export DB_USER="postgres"
-export DB_USER="user"
-export DB_PORT="5432"
-export DB_PASSWORD="password"
-export DB_NAME="database"
